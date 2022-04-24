cc0da4bad68b4cd79313ed55447d7db09aad73af
Wany <sung@wany.io>
2022-04-24 11:54:36 +0900

    init

---
+++ src/main/java/io/wany/amethy/Amethy.java
@@ -0,0 +1,13 @@
+package io.wany.amethy;
+
+/**
+ * Hello world!
+ *
+ */
+public class Amethy 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
