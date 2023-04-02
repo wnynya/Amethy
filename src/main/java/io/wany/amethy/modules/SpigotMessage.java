dff119c86a6e36b770d6d9908acf9fe88be8c638
Wany <sung@wany.io>
2023-04-02 23:00:52 +0900

    add deprecation supresswarnings

--- src/main/java/io/wany/amethy/modules/SpigotMessage.java
+++ src/main/java/io/wany/amethy/modules/SpigotMessage.java
@@ -15,6 +15,7 @@ import java.util.List;
 import java.util.UUID;
 import java.util.regex.Pattern;
 
+@SuppressWarnings("deprecation")
 public class SpigotMessage implements ServerMessage {
 
   private static SpigotMessage THIS;
